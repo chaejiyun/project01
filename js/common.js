f8d864ba2fbebf186aa30fb6e4c60cec25a5b05e
chaejiyun <jy44061@naver.com>
2022-03-02 11:17:09 +0900

    version.0

---
+++ js/common.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".con2", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+
+  var swiper = new Swiper(".con4", {
+    slidesPerView: 4,
+    spaceBetween: 70,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
